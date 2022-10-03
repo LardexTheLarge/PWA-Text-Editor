52f6d1e314c51b52278a529d0f4631dd80262b8a
Gabe Vee <0gabevee0@gmail.com>
2022-10-03 14:51:32 -0600

    added inject manifest code

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,10 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: "./index.html",
       }),
-
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
       new WebpackPwaManifest({
         icons: [
           {
