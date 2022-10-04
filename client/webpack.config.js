3d76954e45aa7996ee9bb0768ac25c2df65bb055
Gabe Vee <0gabevee0@gmail.com>
2022-10-03 18:55:57 -0600

    added correct file path for navbar icon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -55,7 +55,10 @@ module.exports = () => {
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: "asset/resource",
+          loader: "file-loader",
+          options: {
+            outputPath: "src/images",
+          },
         },
         {
           test: /\.m?js$/,
